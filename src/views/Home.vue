5ca20fd317ebe6aaa8ca86edb919334c5b74fa15
ding <walker200665@gmail.com>
2021-03-16 20:12:33 +0800

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -48,58 +48,56 @@ export default {
   },
   created() {
     // 在創建vue實例時連結websocket
-    console.log(process.env.VUE_APP_URL);
-    // this.webSocket();
+    this.webSocket();
   },
   methods: {
-    // webSocket() {
-    //   console.log(process.env.WEBSOCKET_URL);
-    //   const self = this;
-    //   if (typeof WebSocket == "undefined") {
-    //     this.$notify({
-    //       title: "提示",
-    //       message: "當前瀏覽器無法接收socket消息，請使用Google Chrome",
-    //       type: "warning",
-    //       duration: 0,
-    //     });
-    //   } else {
-    //     //socket url and connet
-    //     const socketUrl = process.env.WEBSOCKET_URL;
-    //     const socket = new WebSocket(socketUrl);
-    //     //open socket
-    //     socket.onopen = function() {
-    //       console.log("瀏覽器WebSocket已開啟");
-    //     };
-    //     // listen on socket message
-    //     socket.onmessage = function(msg) {
-    //       // transform message to json format
-    //       const data = JSON.parse(msg.data);
-    //       const dataDate = data.date;
-    //       //限定顯示資料總數（目前為50筆），超過的話就移除首項、push新data到末項
-    //       if (self.option.xAxis.data.length >= 50) {
-    //         setTimeout(() => {
-    //           self.option.xAxis.data.shift();
-    //           self.option.series[0].data.shift();
-    //         }, 0);
-    //         self.option.xAxis.data.push(dataDate);
-    //         self.option.series[0].data.push([
-    //           data.open,
-    //           data.close,
-    //           data.low,
-    //           data.high,
-    //         ]);
-    //       } else {
-    //         self.option.xAxis.data.push(dataDate);
-    //         self.option.series[0].data.push([
-    //           data.open,
-    //           data.close,
-    //           data.low,
-    //           data.high,
-    //         ]);
-    //       }
-    //     };
-    //   }
-    // },
+    webSocket() {
+      const self = this;
+      if (typeof WebSocket == "undefined") {
+        this.$notify({
+          title: "提示",
+          message: "當前瀏覽器無法接收socket消息，請使用Google Chrome",
+          type: "warning",
+          duration: 0,
+        });
+      } else {
+        //socket url and connet
+        const socketUrl = process.env.VUE_APP_URL;
+        const socket = new WebSocket(socketUrl);
+        //open socket
+        socket.onopen = function() {
+          console.log("瀏覽器WebSocket已開啟");
+        };
+        // listen on socket message
+        socket.onmessage = function(msg) {
+          // transform message to json format
+          const data = JSON.parse(msg.data);
+          const dataDate = data.date;
+          //限定顯示資料總數（目前為50筆），超過的話就移除首項、push新data到末項
+          if (self.option.xAxis.data.length >= 50) {
+            setTimeout(() => {
+              self.option.xAxis.data.shift();
+              self.option.series[0].data.shift();
+            }, 0);
+            self.option.xAxis.data.push(dataDate);
+            self.option.series[0].data.push([
+              data.open,
+              data.close,
+              data.low,
+              data.high,
+            ]);
+          } else {
+            self.option.xAxis.data.push(dataDate);
+            self.option.series[0].data.push([
+              data.open,
+              data.close,
+              data.low,
+              data.high,
+            ]);
+          }
+        };
+      }
+    },
   },
 
   components: {
